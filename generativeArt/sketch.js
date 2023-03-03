f5b0b8655eb0a308a596b1fe0d1bc7a885798c5f
Eesha <126822458+yhe-c@users.noreply.github.com>
2023-03-03 15:19:15 -0600

    small cahnges

--- generativeArt/sketch.js
+++ generativeArt/sketch.js
@@ -4,7 +4,7 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("white");
-  lotsOfLines(50, 50)
+  lotsOfLines(50, 50);
 }
 
 function draw() {
@@ -15,7 +15,7 @@ function lotsOfLines(cols, rows) {
   for (let x = 0; x < cols; x ++) {
     for (let y = 0; y< rows; y ++){
       let spaceAmount = width/cols;
-      diagonalLine(x * spaceAmount, y * spaceAmount, spaceAmount)
+      diagonalLine(x * spaceAmount, y * spaceAmount, spaceAmount);
     }
   }
 }
