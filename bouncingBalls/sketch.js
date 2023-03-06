3d0c6e44b1ea5c498d10d39e37c1f9b8c16e202e
Eesha <126822458+yhe-c@users.noreply.github.com>
2023-03-06 15:19:24 -0600

    bouncing balls

---
+++ bouncingBalls/sketch.js
@@ -0,0 +1,47 @@
+// Bouncing Balls
+//Using arrays and object notation
+
+let shapes = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  spawnBall(width/2, height/2);
+}
+
+function draw() {
+  background(220);
+  moveShapes();
+  displayShapes();
+}
+
+function mousePressed() {
+  spawnBall(mouseX, mouseY);
+}
+
+function moveShapes() {
+  for (let i = 0; i < shapes.length; i++) {
+    if (){
+    shapes[i].x += shapes[i].dx;
+    shapes[i].y += shapes[i].dy;
+    }
+  }
+}
+
+function displayShapes() {
+  for (let i = 0; i < shapes.length; i++) {
+    fill(shapes[i].theColor);
+    circle(shapes[i].x, shapes[i].y, shapes[i].diameter);
+  }
+}
+
+function spawnBall(tempX, tempY) {
+  let newBall = {
+    x: tempX,
+    y: tempY,
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    diameter: random(25, 100),
+    theColor: color(random(255), random(255), random(255))
+  };
+  shapes.push(newBall);
+}
