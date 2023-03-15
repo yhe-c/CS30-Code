0f342a999bcf76669c6c1977940ac6f7b384fe74
Eesha <126822458+yhe-c@users.noreply.github.com>
2023-03-15 15:18:50 -0600

    wow

--- arrayAssignment/sketch.js
+++ arrayAssignment/sketch.js
@@ -1,11 +1,16 @@
 // Arrays and Object Notation
 
-let shape = [];
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  fill("yellow");
+  for (let i = 0; i < 10000; i++) {
+    rotate(15);
+    triangle(mouseX + width/4, i+=5, mouseY, i++, mouseX + 250, i+=5);
+  }
+  
 }
