f8fb78987c5e9d47d5bfb761e0571114913a4618
Eesha <126822458+yhe-c@users.noreply.github.com>
2023-03-20 14:19:02 -0600

    wow

--- arrayAssignment/sketch.js
+++ arrayAssignment/sketch.js
@@ -1,28 +1,62 @@
 // Arrays and Object Notation
+// Eesha
+// March 20, 2023
 
-let spirals = [];
+// Extra for Experts Consideration
+// 
+
+
+let spiral = [];
+let reps = 750;
+let count = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES);
+  //angleMode(DEGREES);
   spawnSpiral(width/2, height/2);
 }
 
 function draw() {
-  background(255);
-  displaySpiral();
+  background(0);
+  displaySpiral(count);
 }
 
 function mousePressed() {
   spawnSpiral(mouseX, mouseY);
+  count++;
 }
 
-function displaySpiral() {
-  translate(mouseX, mouseY);
-  for (let i = 0; i < 1000; i++) {
-    rotate(50);
-    fill(spirals[i].theColor);
-    line(spirals[i].x, spirals[i].y, mouseX + 500, mouseY += 505);
+function mouseWheel(event) {
+  if (reps < 250) {
+    reps += 250;
+  }
+  else if (event.delta > 0) {
+    reps -= 5;
+  }
+  else {
+    reps += 5;
+  }
+}
+
+function displaySpiral(count) {
+  translate(spiral[count].x, spiral[count].y);
+  // for (let i = 0; i < reps; i++) {
+  //   rotate(spiral[count].rotation);
+  //   stroke(spiral[count].theColor);
+  //   line(width, i+=5, mouseX - width, mouseY - width/2);
+  // }
+
+  
+  let wave = sin(radians(frameCount));
+  let w = wave * map(mouseX, 0, height, 2500, 0);
+  for (let i = 0; i < 500; i++) {
+    rotate(spiral[count].rotation);
+    stroke(spiral[count].theColor);
+    line(850, i - w/2, -850, i++);
+    stroke(spiral[count].theColor2);
+    line(-850, -i + w, 550, i++);
+    stroke(spiral[count].theColor3);
+    line(-850, i - w, 850, i++);
   }
 }
 
@@ -30,7 +64,10 @@ function spawnSpiral(tempX, tempY) {
   let newSpiral = {
     x: tempX,
     y: tempY,
-    theColor: color(random(255), random(255), random(255))
+    rotation: random(360),
+    theColor: color(random(255), random(255), random(255)),
+    theColor2: color(random(255), random(255), random(255)),
+    theColor3: color(random(255), random(255), random(255))
   };
-  spirals.push(newSpiral);
+  spiral.push(newSpiral);
 }
