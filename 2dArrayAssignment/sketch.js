a2c621463a39fb0287159aae2bf30abb665bb18d
Eesha <126822458+yhe-c@users.noreply.github.com>
2023-03-27 15:19:11 -0600

    start

--- 2dArrayAssignment/sketch.js
+++ 2dArrayAssignment/sketch.js
@@ -1,15 +1,60 @@
-// Project Title
-// Your Name
-// Date
+// 2048 But You Can't Get To 2048
+// Eesha
+// April 6, 2023
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// 
 
+let grid;
+const ROWS = 5;
+const COLS = 5;
+let cellSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = createEmptyGrid(ROWS, COLS);
+  //creating biggest square grid possible
+  if (width < height) {
+    cellSize = width/ROWS;
+  }
+  else {
+    cellSize = height/ROWS;
+  }
 }
 
 function draw() {
   background(220);
+  displayGrid();
+}
+
+function keyTyped() {
+  if(key === "r") { //restart with an empty grid
+    grid = createEmptyGrid(ROWS, COLS);
+  }
+}
+
+function displayGrid() {
+  for(let y = 0; y < ROWS; y++) {
+    for (let x = 0; x < COLS; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("yellow");
+        text("2", )
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function createEmptyGrid(ROWS, COLS) {
+  let newGrid = [];
+  for (let y = 0; y < ROWS; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < COLS; x++) {
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
 }
