f72c9f16f311442db4ef229c9da72c72ba33fdb2
Eesha <126822458+yhe-c@users.noreply.github.com>
2023-03-29 21:00:58 -0600

    ...

--- 2dArrayAssignment/sketch.js
+++ 2dArrayAssignment/sketch.js
@@ -6,8 +6,8 @@
 // 
 
 let grid;
-const ROWS = 5;
-const COLS = 5;
+const ROWS = 4;
+const COLS = 4;
 let cellSize;
 let blockX = 0;
 let blockY = 0;
@@ -25,6 +25,7 @@ function setup() {
 }
 
 function draw() {
+  translate(width/4, 0);
   background(220);
   displayGrid();
 }
@@ -33,41 +34,30 @@ function keyTyped() {
   if (key === "r") { //restart with an empty grid
     grid = createEmptyGrid(ROWS, COLS);
   }
-  else if (key === "UP_ARROW") {
+  else if (key === "UP_ARROW") { //moves all blocks up if possible
     moveBlocks(0, -1);
   }
-  else if (key === "DOWN_ARROW") {
+  else if (key === "DOWN_ARROW") { //moves all blocks down if possible
     moveBlocks(0, 1);
   }
-  else if (key === "LEFT_ARROW") {
+  else if (key === "LEFT_ARROW") { //moves all blocks left if possible
     moveBlocks(-1, 0);
   }
-  else if (key === "RIGHT_ARROW") {
+  else if (key === "RIGHT_ARROW") { //moves all blocks right if possible
     moveBlocks(1, 0);
   }
 }
 
 function moveBlocks(x, y) {
-  while (blockX + x >= 0 && blockX + x < COLS && blockY + y >= 0 && blockY + y < ROWS) {
-    if (grid[blockY + y][blockX + x] === 0) {
-      let tempX = blockX;
-      let tempY = blockY;
-    
-      blockX += x;
-      blockY += y;
-    
-      //update grid
-      grid[blockY][blockX] = 9;
-      grid[tempY][tempX] = 0;
-    }
-  }
+
 }
 
-function displayGrid() {
+function displayGrid() { //functiont that displays the grid
   for(let y = 0; y < ROWS; y++) {
     for (let x = 0; x < COLS; x++) {
       if (grid[y][x] === 0) {
         fill("white");
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[y][x] === 1) {
         fill("yellow");
@@ -78,7 +68,7 @@ function displayGrid() {
   }
 }
 
-function createEmptyGrid(ROWS, COLS) {
+function createEmptyGrid(ROWS, COLS) { //function that restarts the game/resets the grid
   let newGrid = [];
   for (let y = 0; y < ROWS; y++) {
     newGrid.push([]);
