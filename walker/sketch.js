8dedf17bb34ffef9824ec9127efe9a02f81a1c16
Eesha <126822458+yhe-c@users.noreply.github.com>
2023-04-18 14:16:07 -0600

    .

--- walker/sketch.js
+++ walker/sketch.js
@@ -13,41 +13,48 @@ class Walker {
     this.speed = 5;
     this.size = 5;
   }
+
   display() {
     noStroke();
     fill(this.color);
     circle(this.x, this.y, this.size);
   }
+
   move() {
     let choice = random(100);
     if (choice < 25) {
+      //up
       this.y -= this.speed;
     }
     else if (choice < 50) {
+      //down
       this.y += this.speed;
     }
     else if (choice < 75) {
+      //left
       this.x -= this.speed;
     }
     else {
+      //right
       this.x += this.speed;
     }
   }
 }
 
-let dio;
-let jojo;
+let kevin;
+let wasi;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  dio = new Walker(width/2, height/2);
-  jojo = new Walker(200, 400);
-  jojo.color = "blue";
+  kevin = new Walker(width/2, height/2);
+  wasi = new Walker(200, 400);
+  wasi.color = "blue";
 }
 
 function draw() {
-  dio.move();
-  jojo.move();
-  dio.display();
-  jojo.display();
+  kevin.move();
+  wasi.move();
+
+  kevin.display();
+  wasi.display();
 }
